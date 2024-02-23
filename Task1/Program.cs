c3c910a283a735ff23c6cb6479f139483a6af75b
EvgeniyaZ <jalini@mail.ru>
2024-02-24 01:14:13 +0300

    первый коммит

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,3 +1,15 @@
 ﻿
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+
+int value = 100;
+Console.WriteLine(value);
+
+double A= 5.54;
+char B = '!';
+bool C = true;
+
+Console.WriteLine(A);
+Console.WriteLine(B);
+Console.WriteLine(C);
 
