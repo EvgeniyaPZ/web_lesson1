22047d50ce62617d4657671f925e512f3415ae01
EvgeniyaZ <jalini@mail.ru>
2024-02-24 01:28:49 +0300

    первый коммит2

---
+++ Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿int a =1;
+int b =2;
+int sum1 = a+b;
+Console.WriteLine(sum1);
+
+
+long v =8;
+long sum2=v+a;
+Console.WriteLine(sum2);
+
+double d = 2.23;
+double sum3= d+a;
+
+Console.WriteLine(sum3);
+
+int product1=a*b;
+long product2 = a*v;
+double product3 = b*d;
+
+Console.WriteLine(product1);
+Console.WriteLine(product2);
+Console.WriteLine(product3);
