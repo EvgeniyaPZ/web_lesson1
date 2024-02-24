c67fa194f81dc635fbd9f0c76db67ec13b3a9ea5
EvgeniyaZ <jalini@mail.ru>
2024-02-24 14:14:04 +0300

    второе число в степени третьего

---
+++ Task12_pract/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Enter number");
+int num1=Convert.ToInt32(Console.ReadLine());
+//bool result = num1>=1000;
+//if (1000<num1 && num1>=100)
+ if (num1>=100 && num1<=999)
+{
+int a =num1%10;
+int b = num1%100/10;
+double result_f = Math.Pow(b,a);
+Console.WriteLine(result_f);  
+Console.WriteLine(b);
+ Console.WriteLine(a); 
+
+}
+else {Console.Write("Число должно быть трехзначное");}
