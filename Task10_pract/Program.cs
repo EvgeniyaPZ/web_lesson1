f56f9965e0b7a57e91ef06cd60d97bc5199a6436
EvgeniyaZ <jalini@mail.ru>
2024-02-24 12:51:16 +0300

    трехзначные числа

---
+++ Task10_pract/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Enter number");
+int num1=Convert.ToInt32(Console.ReadLine());
+//bool result = num1>=1000;
+if (1000<=num1) 
+{
+    Console.Write("Число должно быть трехзначное");
+return;
+}
+if (num1<100)
+{
+    Console.Write("Число должно быть трехзначное");
+return;
+}
+int a = num1/100;
+int b = num1%10;
+int result_f = a+b;
+Console.WriteLine(result_f);
